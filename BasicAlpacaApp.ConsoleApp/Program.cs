b6e16368b2cced55172aae86f1d0758902648918
zhixian <zhixian@hotmail.com>
2022-05-17 22:28:19 +0800

    init commit

---
+++ BasicAlpacaApp.ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using Alpaca.Markets;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World!");
+
+const string APP_SETTINGS_CONFIGURATION_FILE = "appsettings.json";
+
+const string ALPACA_PAPER_API_KEY_ID_CONFIGURATION_KEY = "alpaca:paper:apiKeyId";
+
+const string ALPACA_PAPER_SECRET_KEY_CONFIGURATION_KEY = "alpaca:paper:secretKey";
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile(APP_SETTINGS_CONFIGURATION_FILE, optional: false, reloadOnChange: true)
+    .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
+    .Build();
+
+string ALPACA_API_KEY_ID = configuration[ALPACA_PAPER_API_KEY_ID_CONFIGURATION_KEY];
+
+string ALPACA_SECRET_KEY = configuration[ALPACA_PAPER_SECRET_KEY_CONFIGURATION_KEY];
+
+var client = Environments
+    .Paper
+    .GetAlpacaTradingClient(new SecretKey(ALPACA_API_KEY_ID, ALPACA_SECRET_KEY));
+
+var clock = await client.GetClockAsync();
+
+if (clock != null)
+{
+    Console.WriteLine(
+        "Timestamp: {0}, NextOpen: {1}, NextClose: {2}",
+        clock.TimestampUtc, clock.NextOpenUtc, clock.NextCloseUtc);
+}
+
+// Get our account information.
+var account = await client.GetAccountAsync();
+
+// Check if our account is restricted from trading.
+if (account.IsTradingBlocked)
+{
+    Console.WriteLine("Account is currently restricted from trading.");
+}
+
+// 200_000
+Console.WriteLine(account.BuyingPower + " is available as buying power.");
+
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # BasicAlpacaApp
+
 A basic .NET Core Alpaca console application use for simple deployments in Kubernetes.
+
+
+```ps1: In C:\src\github.com\ongzhixian\BasicAlpacaApp
+dotnet new sln -n BasicAlpacaApp
+dotnet new console -n BasicAlpacaApp.ConsoleApp
+dotnet sln .\BasicAlpacaApp.sln add .\BasicAlpacaApp.ConsoleApp\
+
+
+dotnet add .\BasicAlpacaApp.ConsoleApp\ package Microsoft.Extensions.Configuration
+dotnet add .\BasicAlpacaApp.ConsoleApp\ package Microsoft.Extensions.Configuration.UserSecrets
+--dotnet add .\BasicAlpacaApp.ConsoleApp\ package Microsoft.Extensions.Configuration.Json
+
+dotnet add .\BasicAlpacaApp.ConsoleApp\ package Alpaca.Markets
+dotnet add .\BasicAlpacaApp.ConsoleApp\ package Alpaca.Markets.Extensions
+
+dotnet user-secrets --project .\BasicAlpacaApp.ConsoleApp\ init
+dotnet user-secrets --project .\BasicAlpacaApp.ConsoleApp\ set "alpaca:paper:apiKeyId" "<api-key-id>"
+dotnet user-secrets --project .\BasicAlpacaApp.ConsoleApp\ set "alpaca:paper:secretKey" "<secret-key>"
+
+```
+
+
+Other ways to extend configuration
+
+```
+
+Microsoft.Extensions.Configuration.CommandLine 
+Microsoft.Extensions.Configuration.Binder 
+Microsoft.Extensions.Configuration.EnvironmentVariables
+```
